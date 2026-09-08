53adb7495f74ed86c0ef06c6f201c2c6f236cfb8
wlamanarchy <walteraguirremazo@gmail.com>
2026-09-08 15:17:21 -0500

    Activa interfaz conectada a Supabase en rama interna

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Observatorio from "./Observatorio.jsx";
+import ObservatorioV3 from "./ObservatorioV3.jsx";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Observatorio />
+    <ObservatorioV3 />
   </React.StrictMode>
 );
